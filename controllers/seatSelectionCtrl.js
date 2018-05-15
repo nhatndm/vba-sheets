2fb9a80eec3b5ebb6fe0aa07ff43a5cda4f6ff64
khanhktong <khanh.tran@dinovative.com>
2018-05-15 10:50:25 +0700

    fix reconnect session

--- controllers/seatSelectionCtrl.js
+++ controllers/seatSelectionCtrl.js
@@ -1,41 +1,100 @@
-const seatHepler = require('../helpers/seat');
+const seatHelper = require('../helpers/seat');
 const SeatType = require('../models/seatType');
+const Seat = require('../models/seat');
 const _ = require('lodash');
 const error = require('../helpers/error');
-const DISABLEDSEAT = 1;
-const ENABLEDSEAT = 0;
+const DISABLED_SEAT = 1;
+const ENABLED_SEAT = 0;
+const CronJob = require('cron').CronJob;
+const axios = require('axios')
+const vbaRailsEndpoint = process.env.RAILS_ENDPOINT
 
 module.exports = (io) => {
+  let listSession = [];
   io.on('connection', (client) => {
     client.on('parse_value', (data) => {
-      let dataResponse = {seat: {
-        status: data.status,
-        name: data.name,
-        nameSeatIndex: data.nameSeatIndex,
-        seatIndex: data.seatIndex,
-        seasonID: client.id
-      }};
-      io.sockets.emit('update_seat', { dataResponse });
-      SeatType.findById(data.data.seat_id, (err, seat) => {
-        if (err) {
-          return console.log(err)
-        } 
-    
-        if (!seat) {
-          return console.log(err)
+      console.log('parse val');
+      if (!client.seat) {
+        client.seat = []
+      }
+      if (data.status === DISABLED_SEAT) {
+        client.seat.push(data.seatId)
+      }
+      let isInSession = false;
+      if (data.status === ENABLED_SEAT) {
+        if (client.seat.indexOf(data.seatId) !== -1) {
+          client.seat.splice(client.seat.indexOf(data.seatId), 1)
+          isInSession = true;
         }
-    
-        let positionEnabled = data.data.position_enabled;
-        let positionDisabled = data.data.position_disabled;
-        seat.seats = seatHepler.generateSpecificSeats(seat.nameSeat ,seat.seats , positionDisabled, DISABLEDSEAT);
-        seat.seats = seatHepler.generateSpecificSeats(seat.nameSeat ,seat.seats , positionEnabled, ENABLEDSEAT);
-      
-        SeatType.findByIdAndUpdate({ _id: seat.id }, { $set: { seats: seat.seats } }, { new: true }, (err, seatSaved) => {
+        else {
+          data.status = DISABLED_SEAT
+        }
+      }
+      let dataResponse = {
+        seat: {
+          status: data.status,
+          seatId: data.seatId
+        }
+      };
+      io.sockets.emit('update_seat', {dataResponse});
+      if (data.status === DISABLED_SEAT || isInSession) {
+        Seat.findByIdAndUpdate(data.seatId, {$set: {status: data.status}}, (err, seatSaved) => {
           if (err) {
             console.log(err)
           }
+          else {
+            console.log('start job')
+            let date = new Date()
+            date.setMinutes(date.getMinutes() + 5)
+            let job = new CronJob(date, function () {
+                axios.get(vbaRailsEndpoint + '/api/check_order?seat_id=' + seatSaved._id)
+                  .then(function (response) {
+                    console.log(response);
+                  })
+                  .catch(function (error) {
+                    console.log('error: ', error)
+                    io.sockets.emit('update_seat', {
+                      seat: {
+                        status: ENABLED_SEAT,
+                        seatId: seatSaved._id
+                      }
+                    });
+                    Seat.findByIdAndUpdate(seatSaved._id, {$set: {status: ENABLED_SEAT}}, (err, seatSaved) => {
+                    })
+                  });
+              }, function () {
+                console.log('done')
+              },
+              true
+            );
+            job.start();
+          }
         })
+      }
+    });
+    client.on('disconnect', () => {
+      console.log('got disconnect')
+      console.log(client.seat)
+      if (client.seat || client.seat > 0) {
+        listSession.push({
+          id: client.id,
+          seat: client.seat
+        })
+      }
+    });
+    client.on('reconnect_session', (session) => {
+      console.log('reconnect');
+      let obj = _.find(listSession, function (o) {
+        return o.id === session
       });
+      if (obj) {
+        client.seat = obj.seat
+        listSession.splice(_.findIndex(listSession, function (o) {
+          return o.id === session
+        }), 1);
+      }
+      else {
+      }
     })
   });
 };
